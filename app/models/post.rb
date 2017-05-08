934326c7d156c549c2f075cf3ba434a2abd1c4f5
sairam illendula <sairam1274@gmail.com>
2017-05-08 18:33:04 +0530

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,3 +13,13 @@ class Post < ActiveRecord::Base
     }
 
 end
+
+  
+    # has_attached_file :photo,
+  #   :storage => :google_drive,
+  #   :google_drive_credentials => "#{Rails.root}/config/google-drive.yml",
+  #   :google_drive_options => {
+  #     :public_folder_id => "0B6_uF-nbe8CdNmRhcjJWb2N2Qnc",
+ 
+  #     :path => proc { |style| "#{style}_#{id}_#{photo.original_filename}" }
+  #   }
